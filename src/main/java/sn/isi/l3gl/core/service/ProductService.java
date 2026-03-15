08d84550876e53d0116f43ccb55f5a046c08ab76
dabandiayefaye <fayedaba608@gmail.com>
2026-03-15 04:10:04 +0000

    listProduct() 0.1.0-snapshot

--- src/main/java/sn/isi/l3gl/core/service/ProductService.java
+++ src/main/java/sn/isi/l3gl/core/service/ProductService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 import sn.isi.l3gl.core.entity.Product;
 import sn.isi.l3gl.core.repository.ProductRepository;
 
+import java.util.List;
+
 @Service
 public class ProductService {
 
@@ -14,6 +16,10 @@ public class ProductService {
     // 0.0.1-SNAPSHOT
     public Product createProduct(Product product) {
         return productRepository.save(product);
+    }
+    // 0.1.0-SNAPSHOT
+    public List<Product> listProducts() {
+        return productRepository.findAll();
     }
 
 }
